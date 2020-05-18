0458e3f065f816bea556602080018a898fc23a1b
Jessica Stone <jstone4815@gmail.com>
2020-05-18 15:21:28 -0700

    add tests for update action

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -49,7 +49,6 @@ describe WorksController do
 
   describe "create" do
     it "can create a new work with valid information accurately, and redirect" do
-      
       # Arrange
       work_hash = {
         work: {
@@ -68,6 +67,7 @@ describe WorksController do
       expect(new_work.title).must_equal work_hash[:work][:title]
       expect(new_work.category).must_equal work_hash[:work][:category]
       expect(new_work.creator).must_equal work_hash[:work][:creator]
+      # expect(flash[:success]).must_include "added successfully"
 
       must_redirect_to work_path(new_work.id)
     end
@@ -105,6 +105,75 @@ describe WorksController do
       must_respond_with :not_found
     end
   end
+
+  describe "update" do
+    it "can update an existing work with valid information accurately, and redirect" do
+      # Assert
+      work_hash = {
+        work: {
+          title: "a new title",
+          category: "movie",
+          creator: "a new creator"
+        },
+      }
+
+      work = Work.first 
+      
+      # Act-Assert
+      expect {
+        patch work_path(work[:id]), params: work_hash
+      }.must_differ "Work.count", 0
+
+      updated_work = Work.find_by(title: work_hash[:work][:title])
+      expect(updated_work.title).must_equal work_hash[:work][:title]
+      expect(updated_work.category).must_equal work_hash[:work][:category]
+      expect(updated_work.creator).must_equal work_hash[:work][:creator]
+
+      expect(flash[:success]).must_include "Successfully updated"
+      must_redirect_to work_path(updated_work.id)
+    end
+
+    it "does not update any work if given an invalid id, and responds with a 404" do
+      # Arrange
+      work_hash = {
+        work: {
+          title: "a new title",
+          category: "movie",
+          creator: "a new creator"
+        },
+      }
+      
+      # Act-Assert
+      expect {
+        patch work_path(97777), params: work_hash
+      }.must_differ "Work.count", 0
+
+      expect(flash[:error]).must_equal "This work does not exist"
+      must_respond_with :not_found
+    end
+
+    it "does not update a work if the form data violates work validations, and responds with a redirect" do
+      work_hash = {
+        work: {
+          title: nil,
+        },
+      }
+       
+      work = Work.first 
+
+      # Act-Assert
+      # Ensure that there is no change in Work.count
+      expect {
+        patch work_path(work[:id]), params: work_hash
+      }.must_differ "Work.count", 0
+
+      # Assert
+      # Check that the controller redirects
+      expect(flash[:error]).must_include "A problem occurred. Could not update"
+      must_respond_with :success
+    end
+  end
+
 
 
 end
