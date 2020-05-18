ca0b0d31f12b18fae51797c8a11aa2ab448fc823
Jessica Stone <jstone4815@gmail.com>
2020-05-18 14:28:33 -0700

    add tests for new and create actions

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -18,4 +18,77 @@ describe WorksController do
       must_respond_with :success
     end
   end
+
+  # describe "show" do
+  #   it "responds with success when showing an existing valid work" do
+  #       # Act
+  #       get work_path(work.id)
+        
+  #       # Assert
+  #       must_respond_with :success
+  #   end
+
+  #   it "responds with 404 with an invalid work id" do
+  #       # Act
+  #       get work_path(99999)
+        
+  #       # Assert
+  #       must_respond_with :not_found
+  #   end
+  # end
+
+  describe "new" do
+    it "responds with success" do
+        # Act
+        get new_work_path
+        
+        # Assert
+        must_respond_with :success
+    end
+  end
+
+  describe "create" do
+    it "can create a new work with valid information accurately, and redirect" do
+      
+      # Arrange
+      work_hash = {
+        work: {
+          title: "new work",
+          category: "new work category",
+          creator: "a creator"
+        },
+      }
+      
+      # Act-Assert
+      expect {
+        post works_path, params: work_hash
+      }.must_change "Work.count", 1
+      
+      new_work = Work.find_by(title: work_hash[:work][:title])
+      expect(new_work.title).must_equal work_hash[:work][:title]
+      expect(new_work.category).must_equal work_hash[:work][:category]
+      expect(new_work.creator).must_equal work_hash[:work][:creator]
+
+      
+      must_respond_with :redirect
+      must_redirect_to work_path(new_work.id)
+    end
+
+    it "does not create a work if the form data violates work validations, and responds with a redirect" do
+      work_hash = {
+        work: {
+          name: "new work",
+          category: nil,
+        },
+      }
+      
+      # Act-Assert
+      expect {
+        post works_path, params: work_hash
+      }.must_differ "Work.count", 0
+            
+      must_respond_with :bad_request
+    end
+  end
+
 end
