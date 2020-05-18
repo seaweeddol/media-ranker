778a84494e890f61fbb371b98893d0e63c3fe1c6
Jessica Stone <jstone4815@gmail.com>
2020-05-18 10:48:08 -0700

    add methods for finding top_work and top_ten_works

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,21 @@
 class Work < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :category, presence: true
   # validates :creator, presence: true
   # validates :publication_year, presence: true
+
+  def self.top_work
+    # TODO add logic to find work with most votes
+    return Work.find(Work.pluck(:id).sample)
+  end
+
+  def self.top_ten_works(category)
+    # find all by category
+    all_works = Work.where(category: category)
+    
+    # sort by votes
+
+    # return first ten items
+    return all_works.sample(10)
+  end
 end
