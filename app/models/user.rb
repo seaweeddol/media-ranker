61f740cb03a2317341a717d4a31d7f607c950ed9
Jessica Stone <jstone4815@gmail.com>
2020-05-19 09:50:58 -0700

    add user and vote models

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :votes
+  validates :name, presence: true, uniqueness: true
+
+end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,5 @@
 class Work < ApplicationRecord
+  has_many :votes
   validates :title, presence: true, uniqueness: true
   validates :category, presence: true
   # validates :creator, presence: true
