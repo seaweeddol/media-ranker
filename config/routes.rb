9f01d8ae95877bfcfd7bb2172eab22b6579d1764
Jessica Stone <jstone4815@gmail.com>
2020-05-19 10:07:06 -0700

    set up routes for user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root "pages#index"
 
   resources :works
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
