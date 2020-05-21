15c9a6f562e1f03776edabe1473c515f070ff684
Jessica Stone <jstone4815@gmail.com>
2020-05-20 19:11:00 -0700

    add perform_login method to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,4 +18,19 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def perform_login(user = nil)
+    user ||= User.create(name: "test user")
+    
+    login_data = {
+      user: {
+        name: user.name,
+      },
+    }
+    post login_path, params: login_data
+
+    # Verify the user ID was saved - if that didn't work, this test is invalid
+    expect(session[:user_id]).must_equal user.id
+
+    return user
+  end
 end
