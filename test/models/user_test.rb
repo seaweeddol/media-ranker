a31c2a0db40dd42b48d31c4bb90046f3fbb349f9
Jessica Stone <jstone4815@gmail.com>
2020-05-20 19:24:42 -0700

    add validation and instantiation tests for User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:new_user) {
+    User.new(name: "test user")
+  }
+  it "can be instantiated" do
+    # Assert
+    expect(new_user.valid?).must_equal true
+  end
+
+  it "will have the required fields" do
+    # Arrange
+    new_user.save
+    user = User.first
+    # Assert
+    expect(user).must_respond_to :name
+  end
+
+  describe "validations" do
+    it "must have a name" do
+      # Arrange
+      new_user.name = nil
+
+      # Assert
+      expect(new_user.valid?).must_equal false
+      expect(new_user.errors.messages).must_include :name
+      expect(new_user.errors.messages[:name]).must_equal ["can't be blank"]
+    end
+
+    it "must have a unique title" do
+      new_user.save
+      another_user = User.create(name: "test user")
+
+      expect(another_user.valid?).must_equal false
+      expect(another_user.errors.messages).must_include :name
+      expect(another_user.errors.messages[:name]).must_equal ["has already been taken"]
+    end
+
+  end
+
 end
