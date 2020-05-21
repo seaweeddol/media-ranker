e2e09ae3b6d280187cd9997fd9765d92b3740aa3
Jessica Stone <jstone4815@gmail.com>
2020-05-20 20:24:52 -0700

    add create action for votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,13 @@
 class VotesController < ApplicationController
+  def create
+    work = Work.find(params[:work_id])
+    if work.votes.where(user_id: session[:user_id]) == []
+      vote = Vote.create(work_id: params[:work_id], user_id: session[:user_id])
+      flash[:success] = "Successfully upvoted!"
+      redirect_to request.referrer
+    else
+      flash[:error] = "A problem occurred: Could not upvote"
+      redirect_to request.referrer
+    end
+  end
 end
