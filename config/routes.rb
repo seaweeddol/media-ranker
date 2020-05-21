e55b961a569b06d2e9ea359d17b63cd47a7a32d5
Jessica Stone <jstone4815@gmail.com>
2020-05-20 20:24:35 -0700

    add route to create vote for work

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#index"
 
-  resources :works
+  resources :works do
+    resources :votes, only: [:create]
+  end
 
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
