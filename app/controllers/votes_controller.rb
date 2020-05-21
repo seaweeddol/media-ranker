a6e5bd562b0d0526c876ae57b775679c68fca67b
Jessica Stone <jstone4815@gmail.com>
2020-05-20 21:29:44 -0700

    add validation for checking if user is logged in before allowing to vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,17 @@
 class VotesController < ApplicationController
   def create
-    work = Work.find(params[:work_id])
-    if work.votes.where(user_id: session[:user_id]) == []
-      vote = Vote.create(work_id: params[:work_id], user_id: session[:user_id])
-      flash[:success] = "Successfully upvoted!"
-      redirect_to request.referrer
+    if session[:user_id]
+      work = Work.find(params[:work_id])
+      if work.votes.where(user_id: session[:user_id]) == []
+        vote = Vote.create(work_id: params[:work_id], user_id: session[:user_id])
+        flash[:success] = "Successfully upvoted!"
+        redirect_to request.referrer
+      else
+        flash[:error] = "A problem occurred: Could not upvote"
+        redirect_to request.referrer
+      end
     else
-      flash[:error] = "A problem occurred: Could not upvote"
+      flash[:error] = "A problem occurred: You must log in to do that"
       redirect_to request.referrer
     end
   end
