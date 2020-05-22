48c9a4aa5aed0d1fcf91ab6868b2f1519e732fa8
Jessica Stone <jstone4815@gmail.com>
2020-05-22 12:27:58 -0700

    delete all related votes when deleting a user or work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :delete_all
   validates :name, presence: true, uniqueness: true
-
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :delete_all
   validates :title, presence: true, uniqueness: {scope: :category}
   validates :category, presence: true
   # validates :creator, presence: true
