80c947a0627a6efad3f165cf69dfa32833645ae8
Jessica Stone <jstone4815@gmail.com>
2020-05-22 09:31:57 -0700

    add index and show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    user_id = params[:id]
+    @user = User.find_by(id: user_id)
+    if @user.nil?
+      head :not_found
+      return
+    end
+  end
+
   def login_form
     @user = User.new 
   end
